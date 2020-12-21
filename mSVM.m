2c76e3e43549ee5fc010c7d1d2358810c1b5c1c2
Jingwei Too <30698779+JingweiToo@users.noreply.github.com>
2020-12-21 11:11:00 +0800

    Add files via upload

--- mSVM.m
+++ mSVM.m
@@ -1,9 +1,9 @@
 
-function Acc = mSVM(feat,label,kfold)
+function Acc = mSVM(feat,label,kfold)  
+% Data preparation
+fold   = cvpartition(label,'KFold',kfold);
 Afold  = zeros(kfold,1);
 for i = 1:kfold
-  % Data preparation
-  fold     = cvpartition(label,'KFold',kfold);
   trainIdx = fold.training(i); testIdx = fold.test(i);
   xtrain   = feat(trainIdx,:); ytrain  = label(trainIdx);
   xvalid   = feat(testIdx,:);  yvalid  = label(testIdx); 
